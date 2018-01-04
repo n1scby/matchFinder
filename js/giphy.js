0b668883d1686dd4f5ef9384ca3766e0046cf6e3
Nic B <n1scby@yahoo.com>
2018-01-03 19:43:58 -0500

    start of using giphy api

---
+++ js/giphy.js
@@ -0,0 +1,58 @@
+"use strict";
+
+(function(){
+    var searchText = document.getElementById("searchText");
+    var giphyButton = document.getElementById("giphyButton");
+    var giphyURL = "http://api.giphy.com/v1/gifs/translate";
+    var gifOutput = document.getElementById("gifOutput");
+    
+
+    var httpRequest;
+    
+        var makeRequest = function makeRequest(url){
+            httpRequest = new XMLHttpRequest();
+    
+            if(!url){
+                alert("No URL was specified.");
+                return false;
+            }
+    
+            if (!httpRequest){
+                console.log("Error creating XMLHttp Object");
+                alert("Unable to create the AJAX request. ");
+                return false;
+            }
+    
+            httpRequest.onreadystatechange = getData;
+            httpRequest.open("GET", url);
+            httpRequest.send();
+        };
+    
+        var getData = function getData(){
+            console.log(httpRequest.readyState + " : " + httpRequest.status);
+            if(httpRequest.readyState === XMLHttpRequest.DONE){
+                if(httpRequest.status === 200){
+                    // console.log("Got Data: " + httpRequest.responseText);
+                    outputGif(httpRequest.responseText);
+                }else{
+                    alert("Request Failed." + httpRequest.status);
+                }
+            }
+        }
+
+
+
+    giphyButton.addEventListener("click", function(){
+        var urlText = searchText.value.trim().replace(/ /g, "+");
+    //    var url = giphyURL + '?' + 's=' + '"' + urlText + '"' + '&api_key=' + '"' + 'z7zvwGpcF6HMSSSoCOf7MUftXKryEhQi' +'"';
+          var url = giphyURL + '?' + 's=' + urlText + '&api_key='  + 'z7zvwGpcF6HMSSSoCOf7MUftXKryEhQi';
+
+        makeRequest(url);
+    })
+
+var outputGif = function outputGif(data){
+    var gifResponse = JSON.parse(data);
+    gifOutput.innerHTML = gifResponse;
+}
+    
+})()
